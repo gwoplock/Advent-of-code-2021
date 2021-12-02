f5072bd07a870cdb63206a3f693adedea2f6aec6
Garrett Battaglia <Garrett.Battaglia@dialexa.com>
2021-12-02 10:12:59 -0600

    Day 2 done and dusted

---
+++ Day2/Part2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+    FILE* file = stdin;
+
+    if (argc == 2)
+    {
+        file = fopen(argv[1], "r");
+    }
+
+    int depth = 0, aim =0, forward = 0, val;
+    char* dir = malloc(100);
+    while ( fscanf(file, "%s %d\n", dir, &val) != EOF)
+    {
+        printf("got dir: %s, val:%d\n", dir, val);
+        if (strcmp("forward", dir) == 0) {
+            forward += val;
+            depth += aim * val;
+        } else if (strcmp("down", dir) == 0) {
+            aim += val;
+        } else if (strcmp("up", dir) == 0) {
+            aim -= val;
+        } else {
+            printf("unkonwn dir");
+        }
+    }
+    printf("depth: %d, fwd: %d, ans: %d\n", depth, forward, depth * forward);
+}
