a25937faa6e4b6e05dda4de6dbf07b66c068bbd4
Garrett Battaglia <Garrett.Battaglia@dialexa.com>
2021-12-06 08:59:52 -0600

    Day 5

---
+++ Day5/Part1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int min(int a, int b)
+{
+    return a > b ? b : a;
+}
+
+int max(int a, int b)
+{
+    return a < b ? b : a;
+}
+
+int main(int argc, char *argv[]){
+
+    FILE* file = stdin;
+
+    if (argc == 2)
+    {
+        file = fopen(argv[1], "r");
+    }
+
+    int map[1000][1000] = { 0 };
+    int x1, x2, y1, y2;
+    while (fscanf(file, "%i,%i -> %i,%i\n", &x1, &y1, &x2, &y2) != EOF)
+    {
+        if (y1 == y2){
+            for (int i = min(x1, x2); i <= max(x2, x1); i++){
+                map[i][y1]++;
+            }
+        } else if (x1 == x2){
+            for (int i = min(y1, y2); i <= max(y2, y1); i++){
+                map[x1][i]++;
+            }
+        }
+    }
+
+    int count  = 0;
+    for (int x = 0; x < 1000; x++){
+        for (int y = 0; y < 1000; y++){
+            if (map[x][y] >= 2)
+            {
+                count++;
+            }
+        }
+    }
+    printf("ans: %d\n", count);
+}
