70f5942bdeb9441403022adfce83f437c96e9fbd
Garrett Battaglia <Garrett.Battaglia@dialexa.com>
2021-12-13 10:28:08 -0600

    Day13 works

---
+++ Day13/part2/main.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+
+void printArr(bool dots[2000][2000], int maxX, int maxY, char* fileName){
+    FILE* file = fopen(fileName, "w");
+    
+    fprintf(file, "\n\n\n");
+
+    for (int y = 0; y <= maxY; y++){
+        for (int x = 0 ; x <= maxX; x++){
+            if (dots[x][y]){
+                fprintf(file, "#");
+            } else {
+                fprintf(file, ".");
+            }
+        }
+        fprintf(file, "\n");
+    }
+
+    fprintf(file, "\n\n\n");
+}
+
+int countDots(bool dots[2000][2000], int maxX, int maxY){
+    int count = 0;
+    for (int y = 0; y <= maxY; y++){
+        for (int x = 0; x <= maxX; x++){
+            if (dots[x][y]){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main(int argc, char *argv[]){
+
+
+    FILE* file = fopen(argv[1], "r");
+
+    bool dots[2000][2000] = { false };
+    int x, y;
+    char n;
+    int maxX = 0, maxY = 0;
+    while (fscanf(file, "%i,%i\n", &x, &y) != EOF) {
+        dots[x][y] = true;
+
+        if (x > maxX){
+            maxX = x;
+        }
+        if (y > maxY){
+            maxY = y;
+        }
+
+        if ((n = fgetc(file)) == 'f') {
+            fseek(file, -1, SEEK_CUR);
+            break;
+        } else {
+            fseek(file, -1, SEEK_CUR);
+        }
+    }
+
+    printf("read Dots\n");
+    char direct =0;
+    int line =0;
+    while (fscanf(file, "fold along %c=%i\n", &direct, &line) != EOF){
+        // printArr(dots, maxX, maxY);
+        printf("current Count: %d\n", countDots(dots, maxX, maxY));
+        printf("fold along %c axis @ %i\n", direct, line);
+
+        if (direct == 'y'){
+            for (int y = line; y <= maxY; y++){
+                for (int x = 0; x <= maxX; x++){
+                    if (dots[x][y]){
+                        dots[x][line-(y-line)] = true;
+                        dots[x][y] = false;
+                    }
+                }
+            }
+        } else if (direct == 'x'){
+            for (int x = line; x <= maxX; x++){
+                for (int y= 0; y <= maxY; y++){
+                    if (dots[x][y]){
+                        dots[line-(x-line)][y] = true;
+                        dots[x][y] = false;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("done folding\n");
+    printArr(dots, maxX, maxY, argv[2]);
+    printf("current Count: %d\n", countDots(dots, maxX, maxY));
+}
